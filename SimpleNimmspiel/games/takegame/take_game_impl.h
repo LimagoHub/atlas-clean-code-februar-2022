18de2609a3bac30bc9714e6e7fb489ad4ede984b
Jo Wagner <jowagner@limago.de>
2022-02-22 10:01:24 +0100

    Terminate turn

--- SimpleNimmspiel/games/takegame/take_game_impl.h
+++ SimpleNimmspiel/games/takegame/take_game_impl.h
@@ -10,45 +10,57 @@
 class take_game_impl : public  game{
 private:
     int stoneCount;
-    bool game_over;
+    int turn;
+
+    bool is_game_over() const noexcept{ // Operation
+        return stoneCount <= 0;
+    }
 
     void human_turn() {
-        int turn;
+        if(is_game_over()) return;
+
         while(true) {
             std::cout << " Es gibt " << stoneCount << " Steine. Bitte nehmen Sie 1, 2 oder 3!" << std::endl;
             std::cin >> turn;
             if(turn >= 1 && turn <= 3) break;
             std::cout << "Ungueltiger Zug" << std::endl;
         }
-        stoneCount -= turn;
+        terminate_turn("Human");
     }
     void computer_turn() {
-        if(stoneCount < 1) {
-            game_over = true;
-            std::cout << "Du Loser" << std::endl;
-            return;
-        }
-
-        if(stoneCount == 1) {
-            game_over = true;
-            std::cout << "Du hast nur Glueck gehabt" << std::endl;
-            return;
-        }
+        if(is_game_over()) return;
 
         const std::vector turns {3,1,1,2};
-        int turn = turns[stoneCount % 4];
+        turn = turns[stoneCount % 4];
         std::cout << "Computer nimmt " << turn << " Steine." << std::endl;
-        stoneCount -= turn;
+
+        terminate_turn("Computer");
     }
-    void play_next_round() {
+
+    void terminate_turn(std::string playerName) { // Integration
+        updateBoard();
+        check_losing(playerName);
+    }
+
+    void check_losing(const std::string &playerName) const {
+        if(is_game_over())
+            std::cout << playerName << " hat verloren." << std::endl;
+
+
+    }
+
+    void updateBoard() { stoneCount -= turn; }
+
+    void play_next_round() { // Integration
         human_turn();
         computer_turn();
     }
+
 public:
-    take_game_impl() : stoneCount(23), game_over(false) {}
+    take_game_impl() : stoneCount(23) {}
 
     void play() noexcept override {
-        while(! game_over) {
+        while(! is_game_over()) {
             play_next_round();
         }
     }
