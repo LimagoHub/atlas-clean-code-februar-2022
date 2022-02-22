157f115dbed97ae85391e9f4f4bcfdf6bcc47066
Jo Wagner <jowagner@limago.de>
2022-02-22 14:57:40 +0100

    letzte lösung

--- SimpleNimmspiel/games/abstract_game.h
+++ SimpleNimmspiel/games/abstract_game.h
@@ -5,9 +5,117 @@
 #pragma once
 
 #include "game.h"
+#include "players/game_player.h"
+template<class BOARD, class TURN>
 class abstract_game: public game {
 
+    static const std::string LOSING_MESSAGE;
+    static const std::string ERROR_MESSAGE;
+
+    std::vector<game_player<BOARD,TURN> *> players;
+
+    game_player<BOARD,TURN> *current_player;
+
+    BOARD board;
+    TURN turn;
+
+    void play_next_round() {
+        for(auto  player : get_players()) {
+            current_player = player;
+            execute_players_turn();
+        }
+    }
+
+    void execute_players_turn() { // Integration
+        if(is_game_over()) return;
+        invoke_players_turn();
+        terminate_turn();
+    }
+
+    void invoke_players_turn()  {
+
+        do {
+            turn = current_player->do_turn(board);
+        } while(players_turn_is_invalid());
+
+    }
+    void terminate_turn() { // Integration
+        updateBoard();
+        check_losing();
+    }
+    bool players_turn_is_invalid() const {
+        if (is_turn_valid()) {
+            return false;
+        }
+        print(ERROR_MESSAGE);
+        return true;
+    }
+    void check_losing() const {
+        if(is_game_over())
+            printf(LOSING_MESSAGE,current_player->get_name());
+    }
+
+    void print(std::string message) const{
+        std::cout << message << std::endl;
+    }
+    void printf(std::string format ,std::string message) const{
+        char buffer[100];
+        sprintf(buffer, format.c_str(), message.c_str());
+        print(buffer);
+    }
+    void printf(std::string format ,int value) const{
+        printf(format, std::to_string(value));
+    }
+    int readInt() const{
+        int result;
+        std::cin >> result;
+        return result;
+    }
+protected:
+    game_player<BOARD, TURN> *getCurrentPlayer() const {
+        return current_player;
+    }
+
+    void setBoard(BOARD board) {
+        abstract_game::board = board;
+    }
+
+    void setTurn(TURN turn) {
+        abstract_game::turn = turn;
+    }
+
+
+    BOARD getBoard() const {
+        return board;
+    }
+
+    TURN getTurn() const {
+        return turn;
+    }
+
+
+
+
+    const std::vector<game_player<BOARD,TURN> *> &get_players() const{
+        return players;
+    }
+    virtual void updateBoard() =0;
+    virtual bool is_game_over() const = 0;
+    virtual bool is_turn_valid() const =0;
+public:
+    abstract_game() {}
+
+    void add_player(game_player<BOARD,TURN> *player) {
+        players.push_back(player);
+    }
+    void play() noexcept override {
+        while(! is_game_over()) {
+            play_next_round();
+        }
+    }
 };
+template<class BOARD, class TURN> const std::string abstract_game<BOARD, TURN>::LOSING_MESSAGE{"%s hat verloren"};
+template<class BOARD, class TURN> const std::string abstract_game<BOARD, TURN>::ERROR_MESSAGE{"Ungueltiger Zug"};
 
 
 
--- SimpleNimmspiel/games/takegame/take_game_impl.h
+++ SimpleNimmspiel/games/takegame/take_game_impl.h
@@ -6,122 +6,34 @@
 #include <iostream>
 #include <vector>
 #include <functional>
-#include "../../games/game.h"
+#include "../../games/abstract_game.h"
 #include "players/take_game_player.h"
 
-class take_game_impl : public  game{
-private:
+class take_game_impl : public  abstract_game<int,int>{
 
-    static const std::string LOSING_MESSAGE;
-    static const std::string ERROR_MESSAGE;
-    std::vector<take_game_player *> players;
-    take_game_player *current_player;
-    int stoneCount;
-    int turn;
-
-    void play_next_round() {
-        for(auto  player : get_players()) {
-            current_player = player;
-            execute_players_turn();
-        }
-    }
-
-    void execute_players_turn() { // Integration
-        if(is_game_over()) return;
-        invoke_players_turn();
-        terminate_turn();
-    }
-
-
-
-    void invoke_players_turn()  {
-
-        do {
-            turn = current_player->do_turn(stoneCount);
-        } while(players_turn_is_invalid());
-
-    }
-
-
-
-    bool players_turn_is_invalid() const {
-        if (is_turn_valid()) {
-            return false;
-        }
-        print(ERROR_MESSAGE);
-        return true;
-    }
-
-
-
-
-    void terminate_turn() { // Integration
-        updateBoard();
-        check_losing();
-    }
-
-    void check_losing() const {
-        if(is_game_over())
-            printf(LOSING_MESSAGE,current_player->get_name());
-
-
-    }
-    bool is_turn_valid() const {
-        return turn >= 1 && turn <= 3;
-    }
-
-    void updateBoard() { stoneCount -= turn; }
-
-
-    bool is_game_over() const noexcept{ // Operation
-        return stoneCount <= 0 || get_players().empty();
-    }
-
-
-
-    // TO Do Refactoren
-    void print(std::string message) const{
-        std::cout << message << std::endl;
-    }
-    void printf(std::string format ,std::string message) const{
-        char buffer[100];
-        sprintf(buffer, format.c_str(), message.c_str());
-        print(buffer);
-    }
-    void printf(std::string format ,int value) const{
-       printf(format, std::to_string(value));
-    }
-    int readInt() const{
-        int result;
-        std::cin >> result;
-        return result;
-    }
 protected:
+    bool is_turn_valid() const override {
+        return getTurn() >= 1 && getTurn() <= 3;
+    }
+    void updateBoard() override { setBoard(getBoard() - getTurn()) ; }
 
-    const std::vector<take_game_player *> &get_players() const;
-
+    bool is_game_over() const noexcept override{ // Operation
+        return getBoard() <= 0 || get_players().empty();
+    }
 public:
-    take_game_impl() : stoneCount(23) {}
+    take_game_impl()   {
+        setBoard(23);
+    }
 
-    void add_player(take_game_player *player) {
-        players.push_back(player);
-    }
-    void play() noexcept override {
-        while(! is_game_over()) {
-            play_next_round();
-        }
-    }
+
 
 
 };
 
 
-const std::string take_game_impl::LOSING_MESSAGE{"%s hat verloren"};
-const std::string take_game_impl::ERROR_MESSAGE{"Ungueltiger Zug"};
 
-const std::vector<take_game_player *> &take_game_impl::get_players() const {
-    return players;
-}
+
+
 
 
 
